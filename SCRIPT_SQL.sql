5b89baace890992ff8d8b4efff1c8f76256405b7
EngineerDV <engdv2017@gmail.com>
2020-03-26 09:25:53 -0600

    AÑADIENDO PROYECTO

---
+++ SCRIPT_SQL.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE Universidad_1;
+USE Universidad_1;
+
+CREATE TABLE Maestro
+(Id_Maestro int auto_increment primary key,
+Nombre_Maestro Varchar(60),
+Apellido_Maestro Varchar(60),
+Titulo_Maestro Varchar(50),
+DPI_Maestro int
+)engine=InnoDB;
+
+CREATE TABLE Alumno
+(Id_Alumno int auto_increment primary key,
+Nombre_Alumno Varchar(60),
+Apellido_Alumno Varchar(60),
+Grupo Varchar(5),
+DPI_Alumno int
+)engine=InnoDB;
+
+SELECT*FROM Maestro
