6f7eb7af80dcceb715858b1fef6cf8052939a03b
EngineerDV <engdv2017@gmail.com>
2020-03-26 09:52:04 -0600

    corrida

--- SCRIPT_SQL.sql
+++ SCRIPT_SQL.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE Universidad_1;
 USE Universidad_1;
 
+/*-----		CREATE TABLE MAESTRO 	-----*/
 CREATE TABLE Maestro
 (Id_Maestro int auto_increment primary key,
 Nombre_Maestro Varchar(60),
@@ -9,6 +10,7 @@ Titulo_Maestro Varchar(50),
 DPI_Maestro int
 )engine=InnoDB;
 
+/*-----		CREATE TABLE ALUMNO 	-----*/
 CREATE TABLE Alumno
 (Id_Alumno int auto_increment primary key,
 Nombre_Alumno Varchar(60),
@@ -16,5 +18,3 @@ Apellido_Alumno Varchar(60),
 Grupo Varchar(5),
 DPI_Alumno int
 )engine=InnoDB;
-
-SELECT*FROM Maestro
